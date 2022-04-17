03b21f27ff38e21d75e91df0991805dbb2ba7f0b
korawit01 <60165680+korawit01@users.noreply.github.com>
2022-04-17 21:49:55 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import pkg from './package'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -46,3 +48,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
+
